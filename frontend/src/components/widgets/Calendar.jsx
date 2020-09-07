f99767376443a03ec4e824f88af7272dcc28a533
Garrett Carder <carder.320@osu.edu>
2020-09-07 03:57:31 +0000

    Week Adjustments by set date TODO: Add Filters and Events to Days

--- frontend/src/components/widgets/Calendar.jsx
+++ frontend/src/components/widgets/Calendar.jsx
@@ -15,33 +15,120 @@ function getEntries() {
     
 }
 
+// Circle Html (after span)
+{/* <div class="available-vendors">
+    <i class="fa fa-circle green"></i>
+    <i class="fa fa-circle yellow"></i>
+</div> */}
 
+// Week of Calendar Component
+function Week(props) {
+    let week = []
+    
+    for (let i of props.days) {
+        console.log(i.date, props.date)
+        if (i.date.getDate() == props.date.getDate() && i.date.getMonth() == props.date.getMonth()) {
+            week.push(
+                <div class="calendar__day today">
+                    <span class="calendar__date">{i.number}</span>
+                </div>
+            )
+        } else if (i.active) {
+            week.push(
+                <div class="calendar__day">
+                    <span class="calendar__date">{i.number}</span>
+                </div>
+            )
+        } else {
+            week.push(
+                <div class="calendar__day inactive">
+                    <span class="calendar__date">{i.number}</span>
+                </div>
+            )
+        }
+    }
 
+    return week
+}
+
+// All Weeks on Calendar Component
+function Weeks(props) {
+    // Declare Weeks Array
+    let weeks = []
+
+    // Loop through Week and add to display
+    for (let i of props.days) {
+        weeks.push(
+            <section class="calendar__week">
+                <Week days={i} date={props.date} />
+            </section>
+        )
+    }
+
+    // Return Weeks
+    return weeks
+}
+
+// Calendar Component
 class Calendar extends Component {
+
+    set_state(state) {
+        if (state.hasOwnProperty('date')) this.state.date = state.date
+        if (state.hasOwnProperty('events')) this.state.events = state.events
+        if (state.hasOwnProperty('filters')) this.state.filters = state.filters
+        if (state.hasOwnProperty('days')) this.state.days = state.days
+    }
+
+    get_state() {
+        return this.state
+    }
+
+    // Create a 5 x 7 array
+    createDays() {
+        // Create array of 5 elements
+        let days = new Array(5)
+
+        // Add 5 Arrays of 7
+        for (let i = 0; i < 5; i++) {
+            days[i] = new Array(7)
+        }
+
+        // Return Array
+        return days
+    }
+
     state = {
+        date: new Date(),
         events: [],
         filters: {
             date: new Date(),
             city: 'Rochester',
             vendor: 'all'
         },
-        days: [
-            [
-
-            ]
-        ]
+        days: this.createDays()
     }
 
     setDays() {
-        let day_index = this.date.getDay()
-        let date = this.date.getDate()
+        // Set Start Calendar Day
+        let firstDayCalendar = new Date(this.state.date.getFullYear(), this.state.date.getMonth(), 0)
+        firstDayCalendar.setDate(firstDayCalendar.getDate() - firstDayCalendar.getDay())
 
-        let week_index = date / 7;
+        // Loop through all days in calendar
+        for (let i = 0; i < 5; i++) {
+            for (let j = 0; j < 7; j++) {
+                // If Past Month
+                if (firstDayCalendar.getDate() > 7 && i < 1) {
+                    this.state.days[i][j] = { active: false, number: firstDayCalendar.getDate(), date: new Date(firstDayCalendar) }
+                } else if (firstDayCalendar.getDate() < 7 && i > 3) {
+                    this.state.days[i][j] = { active: false, number: firstDayCalendar.getDate(), date: new Date(firstDayCalendar) }
+                } else {
+                    this.state.days[i][j] = { active: true, number: firstDayCalendar.getDate(), date: new Date(firstDayCalendar) }
+                }
 
-        let first_index = new Date(this.date)
-        
-
-        this.days[week_index][day_index] = date;
+                // Increment Day
+                firstDayCalendar.setDate(firstDayCalendar.getDate() + 1)
+            }
+        }
     }
 
     componentDidMount() {
@@ -57,211 +144,82 @@ class Calendar extends Component {
     }
 
     render() {
+        this.setDays()
         console.log(this.state)
         return (
             <div style={{paddingTop: '200px', paddingBottom: '80px'}}>
                 <main class="calendar-contain">
-                    <div style={{height: '130px;'}}></div>
 
-                    <section class="title-bar">
-                        <span id="month-selector" class="title-bar__month">
-                            September
-                            <div id="selector-month" class="popup-bar">
-                                <ul>
-                                    <li>January</li>
-                                    <li>February</li>
-                                    <li>March</li>
-                                    <li>April</li>
-                                    <li>May</li>
-                                </ul>
-                            </div>
-                        </span>
-                        <span id="year-selector" style={{marginLeft: '10px'}} class="title-bar__month">
-                            2020
-                            <div id="selector-year" class="popup-bar">
-                                <ul>
-                                    <li>2020</li>
-                                    <li>2021</li>
-                                    <li>2022</li>
-                                    <li>2023</li>
-                                    <li>2024</li>
-                                </ul>
-                            </div>
-                        </span>
-                        <span id="city-selector" style={{marginLeft: '10px'}} class="title-bar__month">
-                            Rochester
-                            <div id="selector-city" class="popup-bar">
-                                <ul>
-                                    <li>Rochester</li>
-                                    <li>Buffalo</li>
-                                    <li>Syracuse</li>
-                                </ul>
-                            </div>
-                        </span>
-                        <span id="vendor-selector" style={{marginLeft: '10px'}} class="title-bar__month">
-                            All
-                            <div id="selector-vendor" class="popup-bar">
-                                <ul>
-                                    <li>Venue <i class="fa fa-circle green"></i></li>
-                                    <li>DJ <i class="fa fa-circle yellow"></i></li>
-                                    <li>Florist <i class="fa fa-circle purple"></i></li>
-                                    <li>Caterer <i class="fa fa-circle orange"></i></li>
-                                </ul>
-                            </div>
-                        </span>
-                    </section>
-
-                    <aside class="calendar__sidebar">
-                        <h2 class="sidebar__heading">Tuesday<br/>September 2</h2>
-                        <ul class="day-events">
-                            <li><i class="fa fa-circle green"></i> Wintergarden</li>
-                            <li><i class="fa fa-circle green"></i> Woodcliffe</li>
-                            <li><i class="fa fa-circle yellow"></i> Marquee DJ</li>
-                        </ul>
-                    </aside>
-
-
-                    <section class="calendar__days">
-                        <section class="calendar__top-bar">
-                        <span class="top-bar__days">Mon</span>
-                        <span class="top-bar__days">Tue</span>
-                        <span class="top-bar__days">Wed</span>
-                        <span class="top-bar__days">Thu</span>
-                        <span class="top-bar__days">Fri</span>
-                        <span class="top-bar__days">Sat</span>
-                        <span class="top-bar__days">Sun</span>
+                        <section class="title-bar">
+                            <span id="month-selector" class="title-bar__month">
+                                September
+                                <div id="selector-month" class="popup-bar">
+                                    <ul>
+                                        <li>January</li>
+                                        <li>February</li>
+                                        <li>March</li>
+                                        <li>April</li>
+                                        <li>May</li>
+                                    </ul>
+                                </div>
+                            </span>
+                            <span id="year-selector" style={{marginLeft: '10px'}} class="title-bar__month">
+                                2020
+                                <div id="selector-year" class="popup-bar">
+                                    <ul>
+                                        <li>2020</li>
+                                        <li>2021</li>
+                                        <li>2022</li>
+                                        <li>2023</li>
+                                        <li>2024</li>
+                                    </ul>
+                                </div>
+                            </span>
+                            <span id="city-selector" style={{marginLeft: '10px'}} class="title-bar__month">
+                                Rochester
+                                <div id="selector-city" class="popup-bar">
+                                    <ul>
+                                        <li>Rochester</li>
+                                        <li>Buffalo</li>
+                                        <li>Syracuse</li>
+                                    </ul>
+                                </div>
+                            </span>
+                            <span id="vendor-selector" style={{marginLeft: '10px'}} class="title-bar__month">
+                                All
+                                <div id="selector-vendor" class="popup-bar">
+                                    <ul>
+                                        <li>Venue <i class="fa fa-circle green"></i></li>
+                                        <li>DJ <i class="fa fa-circle yellow"></i></li>
+                                        <li>Florist <i class="fa fa-circle purple"></i></li>
+                                        <li>Caterer <i class="fa fa-circle orange"></i></li>
+                                    </ul>
+                                </div>
+                            </span>
                         </section>
 
-                        <section class="calendar__week">
-                        <div class="calendar__day inactive">
-                            <span class="calendar__date">30</span>
-                        </div>
-                        <div class="calendar__day inactive">
-                            <span class="calendar__date">31</span>
-                        </div>
-                        <div class="calendar__day">
-                            <span class="calendar__date">1</span>
-                        </div>
-                        <div class="calendar__day">
-                            <span class="calendar__date">2</span>
-                        </div>
-                        <div class="calendar__day">
-                            <span class="calendar__date">3</span>
-                        </div>
-                        <div class="calendar__day">
-                            <span class="calendar__date">4</span>
-                        </div>
-                        <div class="calendar__day">
-                            <span class="calendar__date">5</span>
-                        </div>
+                        <aside class="calendar__sidebar">
+                            <h2 class="sidebar__heading">Tuesday<br/>September 2</h2>
+                            <ul class="day-events">
+                                <li><i class="fa fa-circle green"></i> Wintergarden</li>
+                                <li><i class="fa fa-circle green"></i> Woodcliffe</li>
+                                <li><i class="fa fa-circle yellow"></i> Marquee DJ</li>
+                            </ul>
+                        </aside>
+
+
+                        <section class="calendar__days">
+                            <section class="calendar__top-bar">
+                            <span class="top-bar__days">Mon</span>
+                            <span class="top-bar__days">Tue</span>
+                            <span class="top-bar__days">Wed</span>
+                            <span class="top-bar__days">Thu</span>
+                            <span class="top-bar__days">Fri</span>
+                            <span class="top-bar__days">Sat</span>
+                            <span class="top-bar__days">Sun</span>
                         </section>
 
-                        <section class="calendar__week">
-                        <div class="calendar__day">
-                            <span class="calendar__date">6</span>
-                        </div>
-                        <div class="calendar__day">
-                            <span class="calendar__date">7</span>
-                        </div>
-                        <div class="calendar__day">
-                            <span class="calendar__date">8</span>
-                        </div>
-                        <div class="calendar__day">
-                            <span class="calendar__date">9</span>
-                        </div>
-                        <div class="calendar__day">
-                            <span class="calendar__date">10</span>
-                        </div>
-                        <div class="calendar__day">
-                            <span class="calendar__date">11</span>
-                        </div>
-                        <div class="calendar__day">
-                            <span class="calendar__date">12</span>
-                        </div>
-                        </section>
-
-                        <section class="calendar__week">
-                        <div class="calendar__day">
-                            <span class="calendar__date">13</span>
-                        </div>
-                        <div class="calendar__day">
-                            <span class="calendar__date">14</span>
-                        </div>
-                        <div class="calendar__day today">
-                            <span class="calendar__date">15</span>
-                            <div class="available-vendors">
-                                <i class="fa fa-circle green"></i>
-                                <i class="fa fa-circle yellow"></i>
-                            </div>
-                        </div>
-                        <div class="calendar__day">
-                            <span class="calendar__date">16</span>
-                        </div>
-                        <div class="calendar__day">
-                            <span class="calendar__date">17</span>
-                        </div>
-                        <div class="calendar__day">
-                            <span class="calendar__date">18</span>
-                        </div>
-                        <div class="calendar__day">
-                            <span class="calendar__date">19</span>
-                        </div>
-                        </section>
-
-                        <section class="calendar__week">
-                        <div class="calendar__day">
-                            <span class="calendar__date">20</span>
-                            <br/>
-                            <div class="available-vendors">
-                                <i class="fa fa-circle green"></i>
-                                <i class="fa fa-circle yellow"></i>
-                                <i class="fa fa-circle purple"></i>
-                                <i class="fa fa-circle orange"></i>
-                            </div>
-                        </div>
-                        <div class="calendar__day">
-                            <span class="calendar__date">21</span>
-                        </div>
-                        <div class="calendar__day">
-                            <span class="calendar__date">22</span>
-                        </div>
-                        <div class="calendar__day">
-                            <span class="calendar__date">23</span>
-                        </div>
-                        <div class="calendar__day">
-                            <span class="calendar__date">24</span>
-                        </div>
-                        <div class="calendar__day">
-                            <span class="calendar__date">25</span>
-                        </div>
-                        <div class="calendar__day">
-                            <span class="calendar__date">26</span>
-                        </div>
-                        </section>
-
-                        <section class="calendar__week">
-                        <div class="calendar__day">
-                            <span class="calendar__date">27</span>
-                        </div>
-                        <div class="calendar__day">
-                            <span class="calendar__date">28</span>
-                        </div>
-                        <div class="calendar__day inactive">
-                            <span class="calendar__date">1</span>
-                        </div>
-                        <div class="calendar__day inactive">
-                            <span class="calendar__date">2</span>
-                        </div>
-                        <div class="calendar__day inactive">
-                            <span class="calendar__date">3</span>
-                        </div>
-                        <div class="calendar__day inactive">
-                            <span class="calendar__date">4</span>
-                        </div>
-                        <div class="calendar__day inactive">
-                            <span class="calendar__date">5</span>
-                        </div>
-                        </section>
+                        <Weeks days={this.state.days} date={this.state.date} />
                     </section>
                 </main>
             </div>
